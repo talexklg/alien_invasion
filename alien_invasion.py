93b9a8f308e1b36b6c525bac903f18a875902f2f
TAlex <talex.kaliningrad@gmail.com>
2023-02-14 12:19:15 +0200

    next commit

--- alien_invasion.py
+++ alien_invasion.py
@@ -50,7 +50,7 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_SPACE:
             self._fire_bullet()
-        elif event.key == K_ESCAPE:
+        elif event.key == pygame.K_ESCAPE:
             sys.exit()
 
     def _check_keyup_events(self, event):
